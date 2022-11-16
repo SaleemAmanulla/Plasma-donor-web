9cdb36fcb018553c70dd2bd73099748e0f0eacab
sanjaidebuger <nsksanjai@gmail.com>
2022-11-17 05:19:56 +0530

    validation

--- app.py
+++ app.py
@@ -1,14 +1,60 @@
-from flask import Flask,render_template
+import ibm_db
+import ibm_db_dbi
+import pandas
+from flask import Flask, render_template, request, session
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite://',
+                       echo = False)
+
+dsn_hostname = "2d46b6b4-cbf6-40eb-bbce-6251e6ba0300.bs2io90l08kqb1od8lcg.databases.appdomain.cloud"
+dsn_uid = "zzd89111"
+dsn_pwd = "p7PccD5sBH1chhAO"
+
+dsn_driver = "{IBM DB2 ODBC DRIVER}"
+dsn_database = "BLUDB"
+dsn_port = "32328"
+dsn_protocol = "TCPIP"
+dsn_security = "SSL"
+
+dsn = (
+    "DRIVER={0};"
+    "DATABASE={1};"
+    "HOSTNAME={2};"
+    "PORT={3};"
+    "PROTOCOL={4};"
+    "UID={5};"
+    "PWD={6};"
+    "SECURITY={7};").format(dsn_driver, dsn_database, dsn_hostname, dsn_port, dsn_protocol, dsn_uid, dsn_pwd,dsn_security)
+
+
+
+try:
+    conn = ibm_db.connect(dsn, "", "")
+    print ("Connected to database: ", dsn_database, "as user: ", dsn_uid, "on host: ", dsn_hostname)
+
+except:
+    print ("Unable to connect: ", ibm_db.conn_errormsg() )
+
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template('login.html')
+def login():
+    error = None
+    if request.method == 'POST':
+        username = request.form['uname']
+        password = request.form['password']
+        session['uname'] = request.form['uname']
 
-@app.route("/login")
-def donate():
-     return render_template('login.html')
+        conn = ibm_db.connect(dsn, "", "")
+        pd_conn = ibm_db_dbi.Connection(conn)
+
+        selectQuery = "SELECT * from regtb where UserName='" + username + "' and password='" + password + "'"
+        dataframe = pandas.read_sql(selectQuery, pd_conn)
+
+
+    return render_template('login.html')
 
 @app.route("/home")
 def home():
